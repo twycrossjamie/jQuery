5dc5f0355b2233e590e3a384770eef06ef563616
twycrossjamie <2twycj12@solent.ac.uk>
2017-09-12 22:37:28 +0100

    completed adding item, counting and deleting items

--- js/example.js
+++ js/example.js
@@ -1,5 +1,5 @@
 $(function() {
-//setup
+//SETUP
     var $list, $newItemForm, $newItemButton; 
     var item = ''; //item is an empty string
     $list = $('ul'); //cache the unordered list
@@ -8,8 +8,47 @@ $(function() {
    $('li').hide().each(function(index) { //hide list items
        $(this).delay(450 * index).fadeIn(1600); //fade the list items in
    });
-    //GOT TO HERE ---------------------------------------
-    //item counter 
-    function updateCount()
+    //ITEM COUNTER 
+    function updateCount() { //declare function
+        var items = $('li[class!=complete]').length; //number of items in list by checking if class is complete
+        $('#counter').text(items); //added complete items into counter circle
+    }
+    updateCount(); //call the function 
     
-})
+    //SETUP FORM FOR NEW ITEMS
+    $newItemButton.show(); //show the button
+    $newItemForm.hide(); //hide the form
+    $('#showForm').on('click',function() {
+        $newItemButton.hide(); //hide the button
+        $newItemForm.show(); //show the form
+    });
+    //ADDING A NEW LIST ITEM 
+    $newItemForm.on('submit', function(e) { //when a new item is submitted
+        e.preventDefault(); //prevent form from submitting
+        var text = $('input:text').val(); //get value of text input
+        $list.append('<li>' + text + '</li>'); //add item to end of the list 
+        $('input:text').val(''); //empty the text input
+        updateCount(); //update the count
+                    });
+    //CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
+    $list.on('click', 'li', function() {
+        var $this = $(this); //cache the element in a jQuery object
+        var complete = $this.hasClass('complete'); //is item complete
+        
+        if (complete === true) { //check if item is complete
+            $this.animate({ //if so animate opacity + padding
+                opacity: 0.0,
+                paddingLeft: '+=180'
+            }, 500, 'swing', function() { //use a callback when animation is complete
+                $this.remove(); //
+            });
+        } else { //otherwise indicate it is complete
+            item = $this.text(); //get the text from the list item 
+            $this.remove(); //remove the list item
+            $list //add back to end of list as complete 
+            .append('<li class=\"complete\">' + item + '</li>') //hide it so it can be faded in 
+            .hide().fadeIn(300);
+            updateCount();  //update the counter
+        } //end of else options
+    }); //end of event handler 
+});
